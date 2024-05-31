ee47ec6f4483b3b9d1443e1f7a3a02f2ca3fbaca
EbenAbban <abbaneben07@gmail.com>
2024-05-31 17:03:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the band generator")
+name=input("what is your name")
+city=input("What is the name of the city you grew up in?\n")
+pet=input("what is the name of your pet?\n")
+bandcom=city + pet
+print("Hello " + name + " your band name is " + bandcom + " ! ")
